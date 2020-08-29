2eb777f90aee62d29d273ad904043b7df31e85eb
seonghwan <nsh1998@kookmin.ac.kr>
2020-08-29 14:29:20 +0900

    Add cameraPreview

--- app/src/main/java/com/CVproject/wifi_solution/Fragment_scan.kt
+++ app/src/main/java/com/CVproject/wifi_solution/Fragment_scan.kt
@@ -1,21 +1,31 @@
 package com.CVproject.wifi_solution
 
-import android.content.Context
-import android.graphics.Camera
-import android.hardware.SensorManager
+import android.hardware.Camera
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_fragment_scan.*
+import kotlinx.android.synthetic.main.activity_fragment_scan.view.*
 
 class Fragment_scan : Fragment(){
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    var mCamera : Camera? = null
 
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.activity_fragment_scan, container, false)
+        setupCamera(view)
         return view
     }
 
+
+
+    private fun setupCamera(view: View?) {
+        if(mCamera == null){
+            mCamera = Camera.open()
+        }
+        var cameraPreview = CameraPreview(view?.context, mCamera!!)
+        view?.camera_frameLayout?.addView(cameraPreview)
+    }
 
 
 }
